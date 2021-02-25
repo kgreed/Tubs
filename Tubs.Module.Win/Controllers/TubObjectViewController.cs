c4c24e2c72ca465bcbe61ad0b8184014ed8a03f5
Kirsten Greed <kirsteng@jobtalk.com.au>
2021-02-25 17:48:10 +1100

    View_CurrentObjectChanged

--- Tubs.Module.Win/Controllers/TubObjectViewController.cs
+++ Tubs.Module.Win/Controllers/TubObjectViewController.cs
@@ -6,17 +6,44 @@ namespace Tubs.Module.Win.Controllers
 {
     [System.ComponentModel.DesignerCategory("")]
       public partial class TubObjectViewController : ObjectViewController<ListView, NPTub>
-    {
+      {
+          private RecordsNavigationController navigationController;
+          private void DynamicCollection_FetchObjects(object sender, FetchObjectsEventArgs e)
+          {
+              var o = new NPTub();
+              e.Objects = o.GetData();
+              e.ShapeData = true;
+
+          }
+
         protected override void OnActivated()
         {
             base.OnActivated();
             var os = (NonPersistentObjectSpace) ObjectSpace;
             os.ObjectsGetting += os_ObjectsGetting;
+           
             // Perform various tasks depending on the target View.
             ObjectSpace.Refresh();
             Frame.GetController<FilterController>()?.Active.SetItemValue("Workaround T890466", false);
             Frame.GetController<FilterController>()?.Active.RemoveItem("Workaround T890466");
+            View.ProcessSelectedItem += View_ProcessSelectedItem;
             View.SelectionChanged += View_SelectionChanged;
+        }
+
+        private void View_ProcessSelectedItem(object sender, System.EventArgs e)
+        {
+            var tub = View.CurrentObject as NPTub;
+            tub.Phone.PhoneNumber = $"2000-{tub.Phone.PhoneNumber}";
+        }
+
+        private void NextObjectAction_Execute(object sender, DevExpress.ExpressApp.Actions.SimpleActionExecuteEventArgs e)
+        {
+           Debug.Print("Next");
+        }
+
+        private void NextObjectAction_Executed(object sender, DevExpress.ExpressApp.Actions.ActionBaseEventArgs e)
+        {
+            Debug.Print("Next2");
         }
 
         private void View_SelectionChanged(object sender, System.EventArgs e)
@@ -24,23 +51,30 @@ namespace Tubs.Module.Win.Controllers
             var lv = sender as ListView;
             var tub = lv.CurrentObject as NPTub;
             tub.Phone.PhoneNumber = $"000-{tub.Phone.PhoneNumber}";
+            //navigationController = Frame.GetController<RecordsNavigationController>(); 
+            //navigationController.NextObjectAction.Execute -= NextObjectAction_Execute;
+            //navigationController.NextObjectAction.Execute += NextObjectAction_Execute;
 
         }
+
+         
 
         private void os_ObjectsGetting(object sender, ObjectsGettingEventArgs e)
         {
             var collection = new DynamicCollection((IObjectSpace) sender, e.ObjectType, e.Criteria, e.Sorting,
                 e.InTransaction);
             collection.FetchObjects += DynamicCollection_FetchObjects;
+            collection.ListChanged += Collection_ListChanged;
+           
             e.Objects = collection;
         }
 
-        private void DynamicCollection_FetchObjects(object sender, FetchObjectsEventArgs e)
+        private void Collection_ListChanged(object sender, System.ComponentModel.ListChangedEventArgs e)
         {
-            var o = new NPTub();
-            e.Objects = o.GetData();
-            e.ShapeData = true;
+            Debug.Print("Changed");
         }
+
+
 
         protected override void OnDeactivated()
         {
---
+++ Tubs.Module.Win/Controllers/TubsDetailViewController.cs
@@ -0,0 +1,73 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Tubs.Module.BusinessObjects;
+namespace Tubs.Module.Win.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class TubsDetailViewController : ViewController
+    {
+        private RecordsNavigationController navigationController;
+        public TubsDetailViewController()
+        {
+            
+            InitializeComponent();
+            TargetObjectType = typeof(NPTub);
+            TargetViewType = ViewType.DetailView;
+ 
+        }
+
+        private void NextObjectAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            Debug.Print("Next");
+            var tub = View.CurrentObject as NPTub;
+            tub.Phone.PhoneNumber = $"1000-{tub.Phone.PhoneNumber}"; // fires too late. The editor is already loaded
+        }
+        
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            navigationController = Frame.GetController<RecordsNavigationController>();
+            navigationController.NextObjectAction.Execute += NextObjectAction_Execute; ;
+            View.CurrentObjectChanged += View_CurrentObjectChanged;
+            // Perform various tasks depending on the target View.
+        }
+
+        private void View_CurrentObjectChanged(object sender, EventArgs e)
+        {
+            var v = sender as DetailView;
+            var tub = v.CurrentObject as NPTub;
+            tub.Phone.PhoneNumber = $"300-{tub.Phone.PhoneNumber}";
+        }
+
+        protected override void OnViewChanged()
+        {
+            base.OnViewChanged();
+        }
+
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+            View.CurrentObjectChanged -= View_CurrentObjectChanged;
+        }
+    }
+}
